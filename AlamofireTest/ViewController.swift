35cdf7d2a8614fd0bfa5476a271a702d77231ab6
Demiantcev <kirushakotik@gmail.com>
2022-08-04 15:13:06 +0500

    Initial Commit

---
+++ AlamofireTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireTest
+//
+//  Created by Кирилл Демьянцев on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
